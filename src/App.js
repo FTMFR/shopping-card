9d6b37b3f074a34318c28d644b23ba7bb71e8ea4
FTMFR <fatemefarajzade018@gmail.com>
2024-04-12 18:35:55 +0330

    Add 'All Products' in App.js file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
+
+      <main className="pb-5">
+        <div className="container">
+          <h1 className="text-center main-title">All Products</h1>
+        </div>
+      </main>
     </div>
   );
 }
