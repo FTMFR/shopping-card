77cebbc7e57296d36e1482b8c3499f8ec97ad575
FTMFR <fatemefarajzade018@gmail.com>
2024-04-14 18:44:25 +0330

    Refactor ProductSection component.
    -Optimize ProductSection component, Handle map method in App.js component in this component, after add context api to app.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import ProductSection from "./Components/Navbar/ProductSection/ProductSection";
+import ProductSection from "./Components/ProductSection/ProductSection";
 import products from "./Components/Products/Products";
 import Toast from "./Components/Toast/Toast";
 import Card from "./Components/Card/Card";
+import ProductsContext from "./Contexts/ProductContext";
 
 function App() {
   const [allProducts, setAllproducts] = useState(products);
+  const [userCart, setUserCart] = useState([]);
+  const [isShowToast, setIsShowToast] = useState(false);
+  const [isShowCard, setIsShowCard] = useState(false);
 
   return (
-    <div>
-      <Navbar />
-      <Card />
-      <main className="pb-5">
-        <div className="container">
-          <h1 className="text-center main-title mt-4">All Products</h1>
-          {allProducts.map((product) => (
-            <ProductSection {...product} />
-          ))}
-        </div>
-      </main>
-      <Toast />
-    </div>
+    <ProductsContext.Provider
+      value={{
+        allProducts,
+        userCart,
+        setUserCart,
+        isShowToast,
+        setIsShowToast,
+        isShowCard,
+        setIsShowCard,
+      }}
+    >
+      <div>
+        <Navbar />
+        <Card />
+        <main className="pb-5">
+          <div className="container">
+            <h1 className="text-center main-title mt-4">All Products</h1>
+            <ProductSection />
+          </div>
+        </main>
+        <Toast />
+      </div>
+    </ProductsContext.Provider>
   );
 }
 
--- src/Components/ProductSection/ProductSection.jsx
+++ src/Components/ProductSection/ProductSection.jsx
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./productSection.css";
+import ProductsContext from "../../Contexts/ProductContext";
 
 const ProductSection = ({ title, infos }) => {
+  const contextData = useContext(ProductsContext);
+
   return (
-    <div className="row justify-content-center mt-5">
-      <h3 className="text-center">{title}</h3>
-      {infos.map((product) => (
-        <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
-          <div className="card py-3 px-3">
-            <div className="col-12 text-center">
-              <img
-                src="/images/1.jpg"
-                alt="Product"
-                className="card-img-top w-75"
-              />
+    <>
+      {contextData.allProducts.map((productSection) => (
+        <div className="row justify-content-center mt-5">
+          <h3 className="text-center">{productSection.title}</h3>
+          {productSection.infos.map((product) => (
+            <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
+              <div className="card py-3 px-3">
+                <div className="col-12 text-center">
+                  <img
+                    src="/images/1.jpg"
+                    alt="Product"
+                    className="card-img-top w-75"
+                  />
+                </div>
+                <div className="card-body text-center">
+                  <p className="card-text">{product.title}</p>
+                  <p className="price">{product.price}$</p>
+                  <br />
+                  <a href="/" className="btn btn-danger">
+                    Add to card
+                  </a>
+                  <a
+                    href="/"
+                    className="btn btn-outline-dark mt-3 text-capitalize"
+                  >
+                    More Information
+                  </a>
+                  <p className="number mt-3">Number: {product.number}</p>
+                </div>
+              </div>
             </div>
-            <div className="card-body text-center">
-              <p className="card-text">{product.title}</p>
-              <p className="price">{product.price}$</p>
-              <br />
-              <a href="/" className="btn btn-danger">
-                Add to card
-              </a>
-              <a href="/" className="btn btn-outline-dark mt-3 text-capitalize">
-                More Information
-              </a>
-              <p className="number mt-3">Number: {product.number}</p>
-            </div>
-          </div>
+          ))}
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
