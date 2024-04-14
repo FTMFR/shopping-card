7d5ec26783cf6248191f7c94e6945a654b481d11
FTMFR <fatemefarajzade018@gmail.com>
2024-04-14 17:56:11 +0330

    Add Products component.
    -Implement static info

---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,42 @@
+const Products = [
+  {
+    title: "Phones",
+    infos: [
+      {
+        id: 1,
+        title: "Samsung A53",
+        img: "/images/1.jpg",
+        price: 12_000_000,
+        number: 34,
+      },
+      {
+        id: 2,
+        title: "Nukia 0011",
+        img: "/images/1.jpg",
+        price: 2_000_000,
+        number: 104,
+      },
+    ],
+  },
+  {
+    title: "Laptops",
+    infos: [
+      {
+        id: 1,
+        title: "Asus vivobook",
+        img: "/images/1.jpg",
+        price: 30_000_000,
+        number: 4,
+      },
+      {
+        id: 2,
+        title: "Lenovo Z6015A",
+        img: "/images/1.jpg",
+        price: 50_000_000,
+        number: 80,
+      },
+    ],
+  },
+];
+
+export default Products;
