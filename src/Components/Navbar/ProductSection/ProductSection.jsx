3752549dc3d55ab52d21b299facb0d34d4724d29
FTMFR <fatemefarajzade018@gmail.com>
2024-04-14 17:55:27 +0330

    Modify ProductSection.
    -Add infos prop to component

--- src/Components/Navbar/ProductSection/ProductSection.jsx
+++ src/Components/Navbar/ProductSection/ProductSection.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import "./productSection.css";
 
-const ProductSection = ({ title }) => {
+const ProductSection = ({ title, infos }) => {
   return (
     <div className="row justify-content-center mt-5">
       <h3 className="text-center">{title}</h3>
-      <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
-        <div className="card py-3 px-3">
-          <div className="col-12 text-center">
-            <img
-              src="/images/1.jpg"
-              alt="Product"
-              className="card-img-top w-75"
-            />
-          </div>
-          <div className="card-body text-center">
-            <p className="card-text">Samsung</p>
-            <p className="price">1200$</p>
-            <br />
-            <a href="/" className="btn btn-danger">
-              Add to card
-            </a>
-            <a href="/" className="btn btn-outline-dark mt-3 text-capitalize">
-              More Information
-            </a>
-            <p className="number mt-3">Number: 15</p>
+      {infos.map((product) => (
+        <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
+          <div className="card py-3 px-3">
+            <div className="col-12 text-center">
+              <img
+                src="/images/1.jpg"
+                alt="Product"
+                className="card-img-top w-75"
+              />
+            </div>
+            <div className="card-body text-center">
+              <p className="card-text">{product.title}</p>
+              <p className="price">{product.price}$</p>
+              <br />
+              <a href="/" className="btn btn-danger">
+                Add to card
+              </a>
+              <a href="/" className="btn btn-outline-dark mt-3 text-capitalize">
+                More Information
+              </a>
+              <p className="number mt-3">Number: {product.number}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
